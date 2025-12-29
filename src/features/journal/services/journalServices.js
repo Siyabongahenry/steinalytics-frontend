b8d73753e97759fae7afcc28936774f2691d2f56
siyabongahenry <siyabongahenry23@gmail.com>
2025-12-29 19:24:32 +0200

    minor update

--- src/features/journal/services/journalServices.js
+++ src/features/journal/services/journalServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:8000";
+const API_BASE = import.meta.env.VITE_API_URL ?? "";
 
 // Map report types to backend endpoints
 const REPORT_ENDPOINTS = {
