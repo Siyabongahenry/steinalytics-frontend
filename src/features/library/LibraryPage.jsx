996f36b45e690ac7b4cf5142a48ad88a159ae6ec
siyabongahenry <siyabongahenry23@gmail.com>
2026-02-04 21:36:55 +0200

    Centered boocard

--- src/features/library/LibraryPage.jsx
+++ src/features/library/LibraryPage.jsx
@@ -80,7 +80,9 @@ export default function LibraryPage() {
           >
             <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
               {books.map((book) => (
-                <BookCard key={book.id} book={book} />
+                <div key={book.id} className="flex justify-center"> 
+                  <BookCard book={book} /> 
+                </div>
               ))}
             </div>
           </InfiniteScroll>
