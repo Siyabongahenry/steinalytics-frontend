ef56b5078b59662dba560a0711238e764fbd0e7b
siyabongahenry <siyabongahenry23@gmail.com>
2026-02-06 21:01:57 +0200

    Updated add book tables to library page

---
+++ src/pages/dashboard/library/LibraryPage.jsx
@@ -0,0 +1,17 @@
+import BookTable from "./BooksTable"
+import BookLibrary from "./BooksTable"
+const LibraryPage = () =>{
+
+    return (
+        <div className="dark-gray-900">
+            <h2>Manage your Library</h2>
+
+            <BookTable/>
+
+
+        </div>
+    )
+
+}
+
+export default LibraryPage
